fc674787e2372f26722df86deadf22b51f2b39c6
Daniel Gonzalez <dggsax@gmail.com>
2020-02-13 18:23:13 -0500

    Initial Commit

---
+++ map.js
@@ -0,0 +1,191 @@
+const classroomHtmlText = `<div class="classroom"> <div class="classroom-row" style="justify-content: start;"> <div class="grid-container section side"> <div class="grid-item" id="table-105">105</div><div class="grid-item" id="table-104">104</div><div class="grid-item" id="table-103">103</div><div class="grid-item" id="table-102">102</div><div class="grid-item" id="table-101">101</div><div class="grid-item" id="table-100">100</div></div><div style="flex-grow: 1;"></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-106">106</div><div class="grid-item" style="order:3;" id="table-107">107</div><div class="grid-item" style="order:5;" id="table-108">108</div><div class="grid-item" style="order:7;" id="table-109">109</div><div class="grid-item" style="order:9;" id="table-110">110</div><div class="grid-item" style="order:11;" id="table-111">111</div><div class="grid-item" style="order:2;" id="table-117">117</div><div class="grid-item" style="order:4;" id="table-116">116</div><div class="grid-item" style="order:6;" id="table-115">115</div><div class="grid-item" style="order:8;" id="table-114">114</div><div class="grid-item" style="order:10;" id="table-113">113</div><div class="grid-item" style="order:12;" id="table-112">112</div></div><div style="flex-grow: 2;"></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-118">118</div><div class="grid-item" style="order:3;" id="table-119">119</div><div class="grid-item" style="order:5;" id="table-120">120</div><div class="grid-item" style="order:7;" id="table-121">121</div><div class="grid-item" style="order:9;" id="table-122">122</div><div class="grid-item" style="order:11;" id="table-123">123</div><div class="grid-item" style="order:2;" id="table-129">129</div><div class="grid-item" style="order:4;" id="table-128">128</div><div class="grid-item" style="order:6;" id="table-127">127</div><div class="grid-item" style="order:8;" id="table-126">126</div><div class="grid-item" style="order:10;" id="table-125">125</div><div class="grid-item" style="order:12;" id="table-124">124</div></div><div style="flex-grow: 2.25;"></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-130">130</div><div class="grid-item" style="order:3;" id="table-131">131</div><div class="grid-item" style="order:5;" id="table-132">132</div><div class="grid-item" style="order:7;" id="table-133">133</div><div class="grid-item" style="order:9;" id="table-134">134</div><div class="grid-item" style="order:11;" id="table-135">135</div><div class="grid-item" style="order:2;" id="table-141">141</div><div class="grid-item" style="order:4;" id="table-140">140</div><div class="grid-item" style="order:6;" id="table-139">139</div><div class="grid-item" style="order:8;" id="table-138">138</div><div class="grid-item" style="order:10;" id="table-137">137</div><div class="grid-item" style="order:12;" id="table-136">136</div></div><div style="flex-grow: 2.25;"></div><div class="eds" style="float:right;"> Cypress Engineering Design Studio </div></div><div class="classroom-row"> <div class="walls" style="flex-grow: 2.5;"></div><div class="hall"></div><div class="walls" style="flex-grow: 1.75;"></div><div class="hall"></div><div class="walls" style="flex-grow: 1.75;"></div><div class="hall"></div><div class="walls" style="flex-grow: 3;"></div></div><div class="classroom-row"> <div class="class-entrance" style="float:right;"> Classroom Entrance </div></div><div class="classroom-row"> <div class="grid-container section side"> <div class="grid-item" id="table-0">0</div><div class="grid-item" id="table-1">1</div><div class="grid-item" id="table-2">2</div><div class="grid-item" id="table-3">3</div><div class="grid-item" id="table-4">4</div><div class="grid-item" id="table-5">5</div><div class="grid-item" id="table-6">6</div></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-12">12</div><div class="grid-item" style="order:3;" id="table-11">11</div><div class="grid-item" style="order:5;" id="table-10">10</div><div class="grid-item" style="order:7;" id="table-9">9</div><div class="grid-item" style="order:2;" id="table-13">13</div><div class="grid-item" style="order:4;" id="table-14">14</div><div class="grid-item" style="order:6;" id="table-15">15</div><div class="grid-item" style="order:8;" id="table-16">16</div></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-24">24</div><div class="grid-item" style="order:3;" id="table-23">23</div><div class="grid-item" style="order:5;" id="table-22">22</div><div class="grid-item" style="order:7;" id="table-21">21</div><div class="grid-item" style="order:2;" id="table-25">25</div><div class="grid-item" style="order:4;" id="table-26">26</div><div class="grid-item" style="order:6;" id="table-27">27</div><div class="grid-item" style="order:8;" id="table-28">28</div></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-38">38</div><div class="grid-item" style="order:3;" id="table-37">37</div><div class="grid-item" style="order:5;" id="table-36">36</div><div class="grid-item" style="order:7;" id="table-35">35</div><div class="grid-item" style="order:2;" id="table-39">39</div><div class="grid-item" style="order:4;" id="table-40">40</div><div class="grid-item" style="order:6;" id="table-41">41</div><div class="grid-item" style="order:8;" id="table-42">42</div></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-49">49</div><div class="grid-item" style="order:3;" id="table-48">48</div><div class="grid-item" style="order:5;" id="table-47">47</div><div class="grid-item" style="order:7;" id="table-46">46</div><div class="grid-item" style="order:2;" id="table-50">50</div><div class="grid-item" style="order:4;" id="table-51">51</div><div class="grid-item" style="order:6;" id="table-52">52</div><div class="grid-item" style="order:8;" id="table-53">53</div></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-60">60</div><div class="grid-item" style="order:3;" id="table-59">59</div><div class="grid-item" style="order:5;" id="table-58">58</div><div class="grid-item" style="order:7;" id="table-57">57</div><div class="grid-item" style="order:2;" id="table-61">61</div><div class="grid-item" style="order:4;" id="table-62">62</div><div class="grid-item" style="order:6;" id="table-63">63</div><div class="grid-item" style="order:8;" id="table-64">64</div></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-74">74</div><div class="grid-item" style="order:3;" id="table-73">73</div><div class="grid-item" style="order:5;" id="table-72">72</div><div class="grid-item" style="order:7;" id="table-71">71</div><div class="grid-item" style="order:2;" id="table-75">75</div><div class="grid-item" style="order:4;" id="table-76">76</div><div class="grid-item" style="order:6;" id="table-77">77</div><div class="grid-item" style="order:8;" id="table-78">78</div></div><div class="grid-container section side"> <div class="grid-item" id="table-99">99</div><div class="grid-item" id="table-98">98</div><div class="grid-item" id="table-97">97</div><div class="grid-item" id="table-96">96</div><div class="grid-item" id="table-96">96</div><div class="grid-item" id="table-95">95</div><div class="grid-item" id="table-94">94</div><div class="grid-item" id="table-93">93</div></div></div><div class="classroom-row bottom"> <div class="grid-container section corner" style="height:inherit;"> <div class="grid-item" id="table-07">07</div><div class="grid-item" id="table-08">08</div><div class="grid-item" id="table-17">17</div></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-20">20</div><div class="grid-item" style="order:3;" id="table-19">19</div><div class="grid-item" style="order:5;" id="table-18">18</div><div class="grid-item" style="order:2;" id="table-29">29</div><div class="grid-item" style="order:4;" id="table-30">30</div><div class="grid-item" style="order:6;" id="table-31">31</div></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-34">34</div><div class="grid-item" style="order:3;" id="table-33">33</div><div class="grid-item" style="order:5;" id="table-32">32</div><div class="grid-item" style="order:2;" id="table-43">43</div><div class="grid-item" style="order:4;" id="table-44">44</div><div class="grid-item" style="order:6;" id="table-45">45</div></div><div class="staff-area"> Staff Area </div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-56">56</div><div class="grid-item" style="order:3;" id="table-55">55</div><div class="grid-item" style="order:5;" id="table-54">54</div><div class="grid-item" style="order:2;" id="table-65">65</div><div class="grid-item" style="order:4;" id="table-66">66</div><div class="grid-item" style="order:6;" id="table-67">67</div></div><div class="grid-container section main"> <div class="grid-item" style="order:1;" id="table-70">70</div><div class="grid-item" style="order:3;" id="table-69">69</div><div class="grid-item" style="order:5;" id="table-68">68</div><div class="grid-item" style="order:2;" id="table-79">79</div><div class="grid-item" style="order:4;" id="table-80">80</div><div class="grid-item" style="order:6;" id="table-81">81</div></div><div class="grid-container section corner" style="height:inherit;"> <div class="grid-item" id="table-82">82</div><div class="grid-item" id="table-91">91</div><div class="grid-item" id="table-92">92</div></div></div></div><br>`;
+const classroomStyleText = `.classroom{margin: auto; max-width: 900px; overflow: scroll; border: 2px dashed #909090;}.bottom{transform: translateY(-30px); margin-bottom: -30px;}.classroom-row{display: flex; justify-content: space-between; flex: 1;}.walls{height: 30px; background-color: #909090;}.hall{flex-grow: 1;}.grid-container{display: grid; align-items: flex-start !important;}.staff-area{width: fit-content; background-color: #3F8CB8; border: 1px solid; height: 100px;}.eds{margin-left: auto; padding: 5px; background-color: #3F8CB8; border: 1px solid; width: 200px; box-sizing: border-box;}.class-entrance{margin-left: auto; padding: 5px; background-color: #3F8CB8; border: 1px solid; margin-bottom: 10px;}.section{height: 100%;}.bottom>.main{margin-top: auto;}.main{grid-template-columns: auto auto; align-items: flex-start;}.side{grid-template-columns: auto;}.corner{grid-template-columns: auto auto auto; align-content: flex-end;}.grid-item{background-color: #DCFFDD; height: 25px; width: 40px; border: 1px solid; text-align: center; position: relative;}.grid-item:hover{background-color: #73FD8A;}@keyframes border-pulsate{0%{outline-width: 2px;}50%{outline-width: 4px;}100%{outline-width: 2px;}}.selected{background-color: #FED9DA !important; border-color: #FC0D1B; outline-color: #FC0D1B; outline-style: solid; outline-width: 2px; z-index: 3000; animation: border-pulsate 0.5s infinite; -webkit-animation: border-pulsate 0.5s infinite;}.selected:hover{-webkit-animation: none; animation: none;}`;
+/**
+ * "Enum" to store which state we're in (this will become useful
+ * once functionality is implemented to have the extension show
+ * all active people on queue in the atble)
+ */
+const viewState = {
+    MAIN_QUEUE: 'queue',
+    STUDENT_CLAIMED: 'claimed'
+}
+
+let currentState = viewState.MAIN_QUEUE;
+let lastActive;
+
+// Given a table number, style that table as "selected"
+function selectTable(tableNumber) {
+    let table = document.getElementById("table-" + tableNumber);
+
+    table.classList.add('selected');
+}
+
+// Given a table number, remove that table's styling
+function unselectTable(tableNumber) {
+    let table = document.getElementById("table-" + tableNumber);
+
+    table.classList.remove('selected');
+}
+/**
+ * Given an array of ids represented as strings, confirm that each
+ * of those IDs exist on the DOM. If one of those ID's don't exist,
+ * then return false, otherwise, return true.
+ * 
+ * @param {Array[string]} idArray 
+ */
+function checkForIds(idArray) {
+    let valid = true;
+    idArray.forEach(function (id) {
+        // console.log('id', id);
+        if (!document.getElementById(id)) {
+            // console.log('id doesnt exist', id);
+            valid = false;
+        }
+    })
+
+    // if we're here, all ID's exist on the DOM, and we return true
+    return valid;
+}
+
+/**
+ * Right now we just decide between states depending on whether
+ * we can see the overall queue or not... lazy I know
+ */
+function refreshState() {
+    let idStatus = checkForIds(['queuelist', 'queue_rows']);
+
+    if (idStatus) {
+        currentState = viewState.MAIN_QUEUE;
+    } else {
+        currentState = viewState.STUDENT_CLAIMED;
+    }
+}
+/**
+ * Gets current active table number, returns -1 if no table number
+ * found on the screen
+ * 
+ * NOTE currently checks for classes `text-left` & `h2` which isn't
+ *  good, but I imagine those tags won't change anytime soon haha
+ */
+function getCurrentTableNumber() {
+    let table_id = document.getElementsByClassName('text-left h2');
+    if (table_id.length && table_id.length > 0) {
+        return parseInt(table_id[0].innerText)
+    } else return -1;
+}
+
+/**
+ * Checks if the style sheet rules for the classroom graph have
+ * been added to the DOM or not, if they have not, the function
+ * will insert the styles to <head>.
+ * 
+ * @param {string} stylesId 
+ */
+function insertClassroomStyles(stylesId) {
+    if (!document.getElementById(stylesId)) {
+        var styleSheet = document.createElement("style")
+        styleSheet.type = "text/css";
+        styleSheet.id = stylesId;
+        styleSheet.innerText = classroomStyleText;
+        document.head.appendChild(styleSheet);
+    }
+}
+
+/**
+ * Insert the HTML for the classroom into the dom, if it 
+ * already exists, attempt to unhide the classroom
+ */
+function insertClassroomHTML() {
+    if (!document.getElementById("classroom-map")) {
+        let queueContainer = document.getElementById("queue-container");
+        let classroom = document.createElement('div');
+        classroom.id = "classroom-map";
+        classroom.innerHTML = classroomHtmlText;
+        queueContainer.prepend(classroom);
+    } else {
+        unhideClassroom();
+    };
+}
+
+/**
+ * If classroom html doesn't exist in dom, unhide it
+ */
+function unhideClassroom() {
+    let classroom = document.getElementById("classroom-map");
+
+    // if classroom exists...
+    if (classroom) {
+        classroom.setAttribute("style", "display: initial;");
+    }
+}
+
+/**
+ * If classroom html exists in dom, hide it
+ */
+function hideClassroom() {
+    let classroom = document.getElementById("classroom-map");
+
+    // if classroom exists...
+    if (classroom) {
+        classroom.setAttribute("style", "display: none;");
+    }
+}
+
+function callback(mutationList, observer) {
+    refreshState();
+
+    switch (currentState) {
+        case viewState.MAIN_QUEUE:
+            unselectTable(lastActive);
+            hideClassroom();
+            break;
+        case viewState.STUDENT_CLAIMED:
+            insertClassroomStyles("classroom-styles");
+            insertClassroomHTML();
+            let activeTable = getCurrentTableNumber();
+            selectTable(activeTable);
+            lastActive = activeTable;
+            break;
+        default:
+            break;
+    }
+}
+
+/**
+ * Find the column where queue container lives, and attempt to insert map
+ */
+function map() {
+    let contentContainer = document.getElementsByClassName('col-sm-12')[0];
+
+    if (contentContainer) {
+        // The observer will look for any and all dom changes below the 
+        // div containing the queue stuff, this can probably be simplified,
+        // but as far as I'm aware this is pretty efficient, oh well.
+        const config = { attributes: false, childList: true, subtree: false };
+        const observer = new MutationObserver(callback);
+        observer.observe(contentContainer, config);
+    } else {
+        console.log("[MAP EXTENSION] Unable to display classroom map, is the queue active?");
+        console.log("[MAP EXTENSION] Or maybe you are in student view");
+    }
+}
+
+/**
+ * Wait for the page to be loaded before attempting to insert map
+ * 
+ * @param {fn} fn - function to execute when page loads
+ */
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+docReady(function () {
+    // DOM is loaded and ready for manipulation
+    map();
+});
